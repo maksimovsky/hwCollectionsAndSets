5597e29549396c11f40d111bd850058ceb7d53ad
romax <maksimovskii.roman@yandex.ru>
2023-04-28 10:20:12 +0300

    Initial commit

---
+++ src/test/java/pro/sky/course2/hwcollectionsandsets/HwCollectionsAndSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.hwcollectionsandsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCollectionsAndSetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
